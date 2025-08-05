046ac8080000ee146c7d167f9890efca9b0116a4
SIDR73 <sidharth.raghavan@gmail.com>
2025-08-05 17:17:07 -0400

    Simplify App component to test React mounting without routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,11 @@
-import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import Home from "./pages/home";
-import Team from "./pages/team";
-
-function Router() {
-  return (
-    <Switch>
-      <Route path="/" component={Home} />
-      <Route path="/team" component={Team} />
-      <Route>
-        <div className="min-h-screen bg-kranti-black text-white flex items-center justify-center">
-          <div className="text-center">
-            <h1 className="text-4xl font-bold mb-4">404 - Page Not Found</h1>
-            <p className="text-gray-300">The page you're looking for doesn't exist.</p>
-          </div>
-        </div>
-      </Route>
-    </Switch>
-  );
-}
 
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <Home />
     </QueryClientProvider>
   );
 }
