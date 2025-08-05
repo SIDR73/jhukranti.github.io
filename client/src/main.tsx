a30afaea515dbe424427fdfdacfaa49a020d4606
SIDR73 <sidharth.raghavan@gmail.com>
2025-08-05 17:24:36 -0400

    Add debugging to React mounting process

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,15 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+console.log("main.tsx is executing");
+const rootElement = document.getElementById("root");
+console.log("Root element:", rootElement);
+
+if (rootElement) {
+  const root = createRoot(rootElement);
+  console.log("React root created");
+  root.render(<App />);
+  console.log("App component rendered");
+} else {
+  console.error("Root element not found!");
+}
